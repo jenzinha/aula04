ea7d5de2a00c8bc421a2eae34870089ca0460840
jamili <jamili.amaral@estudante.ifms.edu.br>
2023-08-15 15:47:24 -0400

    Update handlerAcessAPI.js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,7 +1,35 @@
 'use server'
+  
+const list = [
+ {  
+    name: "jamili",
+    email: "mili@gmail.com",
+    password: "123",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+ },
+ {
+    name: "yas",
+    email: "yas@gmail.com",
+    password: "1230",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+ },
+ {
+    name: "jennie",
+    email: "jen@gmail.com",
+    password: "124",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+ }
+];   
+
+
 
 const getUserAuthenticated = (user) => {
 
+    return(
+        <ul>
+            
+        </ul>
+    )
 }
 
 const getUsers = () =>{
