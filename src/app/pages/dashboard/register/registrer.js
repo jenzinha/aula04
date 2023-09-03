f1424d6c5f30ca880dbd0c2e95e43cf57b9b6589
jamili <jamili.amaral@estudante.ifms.edu.br>
2023-09-02 23:30:58 -0400

    começando registro de usuario

---
+++ src/app/pages/dashboard/register/registrer.js
@@ -0,0 +1,48 @@
+'use client'
+import { useState } from "react";
+import { useRouter } from 'next/navigation';
+
+export default function RegisterUsers() {
+    const [user, setUser] = useState({
+      name: '',
+      email: '',
+      password: '',
+    })
+
+    return (
+        <div>
+      <h1>Cadastro de Usuário</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nome">Nome:</label>
+          <input
+            type="text"
+            placeholder="digite seu nome"
+            name="nome"
+            value={user.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            placeholder="digite seu email"
+            name="email"
+            value={user.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="senha">Senha:</label>
+          <input
+            type="password"
+            placeholder="informe uma senha"
+            name="senha"
+            value={user.password}
+          />
+        </div>
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+}
