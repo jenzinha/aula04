cc32d634dd3a2622c3460959ec4592ee89f90c96
jamili <jamili.amaral@estudante.ifms.edu.br>
2023-09-14 08:59:17 -0400

    fazendo caminho

--- src/middleware.js
+++ src/middleware.js
@@ -13,12 +13,17 @@ export const middleware = (request) => {
             return NextResponse.redirect(urlLogin);
         }    
  
-        if (request.nextUrl.pathname === '/' && token)  
-         return NextResponse.redirect('/pages/dashboard');
+        if (request.nextUrl.pathname === '/' && token){
+         return NextResponse.redirect('/pages/dashboard');     
         }
+        if (request.nextUrl.pathname === '/pages/alter/alter' || request.nextUrl.pathname === '/pages/register/register'){
+            return NextResponse.redirect(urlLogin);
+        }
+    }
+        
     NextResponse.next();
 };
 export const config = {
-    matcher: ['/', '/pages/dashboard']
+    matcher: ['/', '/pages/dashboard', '/pages/alter/alter', '/pages/register/register']
 };
 
