7f59d6e6a940b11a21153a9436cd4d364bcface6
jamili <jamili.amaral@estudante.ifms.edu.br>
2023-09-14 08:38:34 -0400

    fazendo o alter

---
+++ src/app/pages/dashboard/alter/alter.js
@@ -0,0 +1,50 @@
+'use client'
+import { useState } from "react";
+import { useRouter } from 'next/navigation';
+
+export default function AlterarUsers() {
+    const route = useRouter();
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+
+    const alterar = (e) => {
+        e.preventDefault()
+
+        const user = {
+            name: name,
+            password: password,
+            email: email
+        }
+    }
+
+    return (
+        <div className='container'>
+            <h3 className='conta'>Registrar usuarios</h3>
+            
+            <form className='contact_form' onSubmit={alterar}>
+                
+                <div className='nome'> 
+                <input id='texto' placeholder='digite seu nome' type="text" name="name" value={name}
+                onChange={e => setName(e.target.value)}/>  
+                </div>
+
+                <div className='email'>
+                <input placeholder='digite seu email' type="email" name="email" value={email}
+                onChange={e => setEmail(e.target.value)}/>
+                </div>
+
+                <div className='password'>
+                <input id='texto' placeholder='digite uma nova senha' type="text" name="password" value={password}
+                onChange={e => setPassword(e.target.value)}/> 
+                </div>
+
+
+                <div className='submit'> 
+                <button id="form_button" type='submit'>Atualizar</button>
+                </div>   
+            </form>
+            </div>
+    );
+}
